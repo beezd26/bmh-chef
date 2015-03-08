d1174fa2d8c6ec48eae6e8f057a27f6a2c7a0572
Blair Douglass <blair.douglass@digitas.com>
2015-03-08 18:32:29 -0500

    update

--- testapp/recipes/default.rb
+++ testapp/recipes/default.rb
@@ -7,11 +7,18 @@
 # All rights reserved - Do Not Redistribute
 #
 
-git "/var/www/test" do
-     repository "https://github.com/beezd26/TestAPI.git"
-     revision "master"
-     action :export
+if node[:deploy]['appshortname'][:application] =="app_1_prod"
+  Chef::Log.debug("DEPLOYING THE TEST!!!!!!")
+  git "/var/www/test" do
+       repository "https://github.com/beezd26/TestAPI.git"
+       revision "master"
+       action :export
+  end
+  execute "start forever" do
+    command "forever start -w /var/www/test/app.js 4001"
+  end
+  next
 end
-execute "start forever" do
-  command "forever start -w /var/www/test/app.js 4001"
+else
+  Chef::Log.debug("WHAT IS THIS!!!!!!!!")
 end
